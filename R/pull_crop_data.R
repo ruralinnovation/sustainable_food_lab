1f214ff6bd789495af47d1a50a89f0b32343fa07
Drew Rosebush <drew.rosebush@gmail.com>
2023-11-16 07:05:35 -0800

    adding source pull function

---
+++ R/pull_crop_data.R
@@ -0,0 +1,9 @@
+
+pull_crop_data <- function(){
+  params <- yaml::read_yaml(here::here("params.yml"))
+  dta <- data.table::fread(params$source$url)[ STATE_ALPHA %in% params$source$state_abbr & COUNTY_NAME != 'NULL']
+  
+  dta$DOMAINCAT_DESC <- ifelse(dta$DOMAINCAT_DESC == '', NA, dta$DOMAINCAT_DESC)
+  
+  readr::write_csv(dta, here::here("data/2017_agcensus_ia_co.csv"))
+}
